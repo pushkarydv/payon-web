2051e450f6ad892ddd69b1d712fc3c758d45a214
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-07-01 21:48:40 +0530

    added Header with dark and light mode

--- src/components/global/header/index.js
+++ src/components/global/header/index.js
@@ -1,5 +1,40 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { MoonIcon, SunIcon } from "@heroicons/react/outline";
 export default function Header() {
-  return <div>Header</div>;
+  const [dark, setDark] = useState(false);
+  useEffect(() => {
+    // getting if there any mode set by user
+    if (localStorage.getItem("payon_dark") == "dark") {
+      setDark(true);
+      document.documentElement.classList.add("dark");
+    }
+  }, []);
+
+  function changeTheme() {
+    if (dark) {
+      document.documentElement.classList.remove("dark");
+      setDark(false);
+      localStorage.setItem("payon_dark", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      setDark(true);
+      localStorage.setItem("payon_dark", "dark");
+    }
+  }
+  return (
+    <div className="flex flex-row text-4xl font-bold items-center justify-between px-4">
+      <a href="#" className="flex flex-row items-center">
+        <img src="/logo.png" className="h-12 mr-2" alt="" /> Payon
+      </a>
+
+      <button onClick={changeTheme} className="transition active:scale-90">
+        {/* show svg after checking if page is dark or light  */}
+        {dark ? (
+          <SunIcon className="h-8 stroke-[1.5]" />
+        ) : (
+          <MoonIcon className="h-8 stroke-[1.5]" />
+        )}
+      </button>
+    </div>
+  );
 }
