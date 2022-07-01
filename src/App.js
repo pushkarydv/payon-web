ff80e169706a607d44f0178bce5dace660f90556
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-07-01 22:11:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
+import Header from "./components/global/Header";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        {/* if route does not exist the user will be redirected to "/" with the path = "*" below */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <main className="min-h-screen w-full overflow-x-hidden bg-slate-50 text-slate-800 dark:bg-slate-900 dark:text-slate-50">
+      <Header />
+      {/* added routes  */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* if route does not exist the user will be redirected to "/" with the path = "*" below */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
   );
 }
