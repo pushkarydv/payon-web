3b4a51d24ca6f8b0c2aff971eaa28755f69ec182
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-07-02 22:20:26 +0530

    added footer

---
+++ src/components/HomeComponents/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="w-full flex justify-center items-center text-primary dark:text-secondary">
+      <a
+        href="https://github.com/pushkarydv/payon-web"
+        className=" hover:opacity-95 mt-2 flex"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {"</>"} & Crafted with&nbsp;💚 &nbsp;
+      </a>
+    </div>
+  );
+}
