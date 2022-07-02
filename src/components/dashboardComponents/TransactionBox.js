2af2ccb6dacd0c743ef78bee23b0d2a005450ed1
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-07-02 16:39:10 +0530

    transactions section

---
+++ src/components/dashboardComponents/TransactionBox.js
@@ -0,0 +1,29 @@
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/outline";
+import React, { useState } from "react";
+
+export default function TransactionBox({ head, type, children }) {
+  const [open, setOpen] = useState(false);
+  function toggle() {
+    open ? setOpen(false) : setOpen(true);
+  }
+  return (
+    <div className="transition-all my-1 py-1 border-b-2 border-b-slate-100">
+      <div
+        className="flex flex-row justify-between text-lg cursor-pointer"
+        onClick={toggle}
+      >
+        <div className={type === "send" ? "text-red-500" : "text-emerald-500"}>
+          {head}
+        </div>
+        <button>
+          {open ? (
+            <ChevronUpIcon className="w-4" />
+          ) : (
+            <ChevronDownIcon className="w-4" />
+          )}
+        </button>
+      </div>
+      {open && <div className="transition-all">{children}</div>}
+    </div>
+  );
+}
