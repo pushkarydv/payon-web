ab3e5dfc391a671896efa4506473db1a30f4f4ec
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-07-09 16:24:56 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,10 @@ import Features from "../components/HomeComponents/Features";
 import Footer from "../components/HomeComponents/Footer";
 import Hero from "../components/HomeComponents/Hero";
 import OpenSource from "../components/HomeComponents/OpenSource";
+
 export default function Home() {
   return (
     <>
-      {" "}
       <Header />
       <Hero />
       <Features />
